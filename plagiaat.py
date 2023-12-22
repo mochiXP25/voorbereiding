0fce78043b12c5efa636a1858a15f565e65b25c9
mohammed_Asad <144899173+mochiXP25@users.noreply.github.com>
2023-12-22 22:21:15 +0100

    add part2 plagiaat

--- plagiaat.py
+++ plagiaat.py
@@ -1,22 +1,49 @@
 import os
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
+def find_identical_files(folders):
+    matrix_with_comments = {auteur: {andere_auteur: [] for andere_auteur in auteurs} for auteur in auteurs}
+    
+    for i, auteurs in enumerate(auteurs):
+        auteurs_dir = os.path.join(folders, auteurs)
+        if not os.path.exists(auteurs_dir):
+            print(f"Error: Directory for auteurs '{auteurs}' not found.")
+            continue
+
+        auteur_bestanden = os.listdir(auteurs_dir)
+        
+        for other_author in auteurs[i+1:]:
+            andere_auteurs = os.path.join(folders, other_author)
+            if not os.path.exists(andere_auteurs):
+                print(f"Error: Directory for auteurs '{other_author}' not found.")
+                continue
+
+            for file in auteur_bestanden:
+                bestand_pad = os.path.join(auteurs_dir, file)
+                other_file_path = os.path.join(andere_auteurs, file)
+
+                if os.path.isfile(bestand_pad) and os.path.isfile(other_file_path):
+                    content = open(bestand_pad, 'r').read()
+                    other_content = open(other_file_path, 'r').read()
+
+                    if content == other_content:
+                        matrix_with_comments[auteurs][other_author].append(f"Files '{file}' are identical.")
+
+    return matrix_with_comments
+
 auteurs = ["Auteur1", "Auteur2", "Auteur3", "Auteur4"]
+base_directory = input("Enter the path to the folders for analysis: ")
+
+matrix_met_opmerkingen = find_identical_files(base_directory)
 
 alias_mapping = {auteur: f"student_{i + 1}" for i, auteur in enumerate(auteurs)}
-
-matrix_met_opmerkingen = {auteur: {andere_auteur: [] for andere_auteur in auteurs} for auteur in auteurs}
-
-matrix_met_opmerkingen["Auteur1"]["Auteur2"] = ["Opmerking1", "Opmerking2"]
-matrix_met_opmerkingen["Auteur3"]["Auteur1"] = ["Opmerking3"]
-matrix_met_opmerkingen["Auteur4"]["Auteur2"] = ["Opmerking4", "Opmerking5"]
 
 env = Environment(
     loader=FileSystemLoader(os.path.join(os.path.dirname(os.path.abspath(__file__)), "")),
     autoescape=select_autoescape()
 )
 
-output_file_name = input("Voer de gewenste uitvoernaam in (zonder extensie): ")
+output_file_name = input("Enter the desired output filename (without extension): ")
 
 template = env.get_template("outputtemplate.html")
 
@@ -26,4 +53,4 @@ output_file_path = f"{output_file_name}.html"
 with open(output_file_path, "w") as output_file:
     output_file.write(html_output)
 
-print(f"HTML-rapport gegenereerd en opgeslagen als '{output_file_path}'.")
+print(f"HTML report generated and saved as '{output_file_path}'.")
